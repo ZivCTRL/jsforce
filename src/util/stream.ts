a4078d58e4d3c7f8d4a1cd99f204304c4fa0b3ea
Ziv Yunger <ziv@getctrl.co>
2021-06-28 15:12:39 +0300

    fuck me if this works

--- src/util/stream.ts
+++ src/util/stream.ts
@@ -1,4 +1,6 @@
 const Stream = require('stream-browserify');
+const stream = require('readable-stream');
+const Duplex = stream.Duplex;
 
 export function createLazyStream() {
   const ins = new Stream.PassThrough();
@@ -52,62 +54,59 @@ export async function readAll(rs: any) {
   });
 }
 
-class DuplexifiedStream {}
+// class DuplexifiedStream {}
 
-// class DuplexifiedStream extends Duplex {
-//   _writable: Writable;
-//   _readable: Readable;
+class DuplexifiedStream extends Duplex {
+  _writable: any;
+  _readable: any;
 
-//   constructor(
-//     ws: Writable,
-//     rs: Readable,
-//     opts: { writableObjectMode?: boolean; readableObjectMode?: boolean } = {},
-//   ) {
-//     super({
-//       writableObjectMode: opts.writableObjectMode ?? ws.writableObjectMode,
-//       readableObjectMode: opts.readableObjectMode ?? rs.readableObjectMode,
-//     });
-//     this._writable = ws;
-//     this._readable = rs;
-//     ws.once('finish', () => {
-//       this.end();
-//     });
-//     this.once('finish', () => {
-//       ws.end();
-//     });
-//     rs.on('readable', () => {
-//       this._readStream();
-//     });
-//     rs.once('end', () => {
-//       this.push(null);
-//     });
-//     ws.on('error', (err) => this.emit('error', err));
-//     rs.on('error', (err) => this.emit('error', err));
-//   }
+  constructor(
+    ws: any,
+    rs: any,
+    opts: { writableObjectMode?: boolean; readableObjectMode?: boolean } = {},
+  ) {
+    super({
+      writableObjectMode: opts.writableObjectMode ?? ws.writableObjectMode,
+      readableObjectMode: opts.readableObjectMode ?? rs.readableObjectMode,
+    });
+    this._writable = ws;
+    this._readable = rs;
+    ws.once('finish', () => {
+      this.end();
+    });
+    this.once('finish', () => {
+      ws.end();
+    });
+    rs.on('readable', () => {
+      this._readStream();
+    });
+    rs.once('end', () => {
+      this.push(null);
+    });
+    ws.on('error', (err : any) => this.emit('error', err));
+    rs.on('error', (err : any) => this.emit('error', err));
+  }
 
-//   _write(chunk: any, encoding: any, callback: any) {
-//     this._writable.write(chunk, encoding, callback);
-//   }
+  _write(chunk: any, encoding: any, callback: any) {
+    this._writable.write(chunk, encoding, callback);
+  }
 
-//   _read(n: number) {
-//     this._readStream(n);
-//   }
+  _read(n: number) {
+    this._readStream(n);
+  }
 
-//   _readStream(n?: number) {
-//     let data;
-//     while ((data = this._readable.read(n)) !== null) {
-//       this.push(data);
-//     }
-//   }
-// }
+  _readStream(n?: number) {
+    let data;
+    while ((data = this._readable.read(n)) !== null) {
+      this.push(data);
+    }
+  }
+}
 
 export function concatStreamsAsDuplex(
   ws: any,
   rs: any,
   opts?: { writableObjectMode?: boolean; readableObjectMode?: boolean },
 ): any {
-  // return new DuplexifiedStream(ws, rs, opts);
-  console.log(ws, rs, opts);
-
-  return new DuplexifiedStream();
+  return new DuplexifiedStream(ws, rs, opts);
 }
